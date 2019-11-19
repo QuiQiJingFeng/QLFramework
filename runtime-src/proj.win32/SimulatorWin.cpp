b025b945444e32763b30d6d3a8b50f21507502d6
zhaoqinglong <zhaoqinglong@lohogames.com>
2019-11-19 17:44:53 +0800

    feat:开启命令行输出窗口

--- runtime-src/proj.win32/SimulatorWin.cpp
+++ runtime-src/proj.win32/SimulatorWin.cpp
@@ -56,7 +56,8 @@
 #include "platform/win32/PlayerMenuServiceWin.h"
 
 // define 1 to open console ui and setup windows system menu, 0 to disable
-#define SIMULATOR_WITH_CONSOLE_AND_MENU 0
+//是否开启命令行输出窗口
+#define SIMULATOR_WITH_CONSOLE_AND_MENU 1
 
 USING_NS_CC;
 
