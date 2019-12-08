785ca16718fc05b9f174aee3da193a7f74d52e7a
JingFeng <gejinnian224@163.com>
2019-12-09 01:04:12 +0800

    feat:ios 解密+解压缩

--- runtime-src/Classes/common/Utils.h
+++ runtime-src/Classes/common/Utils.h
@@ -19,14 +19,8 @@ public:
     bool versionGreater(const std::string& version1, const std::string& version2);
     long xxteaDecrypt(unsigned char* bytes,long size,char* xxteaSigin,char* xxteaKey);
     
-    // wav转mp3
-    bool convertWavToMp3(string wavPath,string mp3Path);
-    FValue convertWavToMp3(FValueVector vector);
-    
     static Utils* getInstance();
-    // 导入PlatformSDK.h头文件,并且实现registerFunc方法,那么就可以在Lua中直接调用
     void registerFunc(){
-        REG_OBJ_FUNC(Utils::convertWavToMp3,Utils::getInstance(),Utils::,"Utils:convertWavToMp3")
     }
 
 private:
